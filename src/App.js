56d4c32a087077b902f43b012cc541afc37a76b6
hendryprasetyo <hendryprastyo13@gmail.com>
2023-01-31 08:49:48 +0700

    first

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import Calculator from './components/calculator'
+import Card from './components/card'
+
+function App(){
+  return(
+    <>
+    <Calculator/>
+    <Card title="air jordan1" desk="ini adalah air jordan 1 yang terbaru"/>
+    <Card title="airjordan 2" desk="ini adalah airjordan 2paling anyar"/>
+    </>
+  )  
+}
+
+export default App
